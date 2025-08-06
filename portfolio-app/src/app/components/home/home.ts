109937aecc062317b59a12722fb204dfcef0f1d0
Atharva.Gulhane <gulhaneatharva@gmail.com>
2025-08-07 00:41:10 +0530

    Added more Data to home page

--- portfolio-app/src/app/components/home/home.ts
+++ portfolio-app/src/app/components/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Hobbies } from '../hobbies/hobbies';
+import { Projects } from '../projects/projects';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,Hobbies,Projects],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
